c095d7c1201aef8fa79645ae3cd9ef67cb63a59d
Robert Petersson <rpetersson@gmail.com>
2022-02-20 22:03:44 +0100

    Three layer architecture

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+# diagram.py
+import imp
+import webbrowser
+from diagrams import Diagram,Cluster
+from diagrams.azure.compute import VM
+from diagrams.azure.database import SQLVM
+from diagrams.azure.network import LoadBalancers
 
-with Diagram("Grouped Workers", show=False, direction="TB"):
-    ELB("lb") >> [EC2("worker1"),
-                  EC2("worker2"),
-                  EC2("worker3"),
-                  EC2("worker4"),
-                  EC2("worker5")] >> RDS("events")
+with Diagram("Simple AZ Diagram", show=False):
+    lb1 = LoadBalancers("LB")
+    lb2 = LoadBalancers("LB")
+
+    with Cluster("Tier 1"):
+        firstTier = [VM("Webserver 1"),VM("Webserver 2"), VM("Webserver 3"),]
+    with Cluster("Tier 2"):
+        secondTier = [VM("Logic 1"),VM("Logic 2"),VM("Logic 3"),]
+    with Cluster("Tier 3"):
+        thirdTier = [SQLVM("DB 1"),SQLVM("DB 2"),SQLVM("DB 3"),]
+    
+    firstTier >> lb1 >> secondTier >> lb2 >> thirdTier    
+    
